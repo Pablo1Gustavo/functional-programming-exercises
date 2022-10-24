15bb7c2d149c3d3d11d87f24d682b0c2a1c43dce
Pablo Gustavo <pablopgfm@gmail.com>
2022-10-24 00:24:49 -0300

    Start ExMaybe with some functions.
    Implements isJust, isNothing, fromJust, catMaybes, fromMaybe and listToMaybe.

---
+++ ex/ExMaybe.hs
@@ -0,0 +1,47 @@
+module ExMaybe where
+
+-- Do not alter this import!
+import Prelude hiding ( maybe, Maybe(..) )
+import qualified Data.Maybe as M
+
+data Maybe a = Nothing | Just a
+    deriving (Show, Eq, Ord)
+
+isJust :: Maybe a -> Bool
+isJust Nothing = False
+isJust _ = True
+
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing _ = False
+
+fromJust :: Maybe a -> a
+fromJust Nothing = error "Can not get value from Nothing"
+fromJust (Just x) = x
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes [] = []
+catMaybes (x : xs)
+    | isJust x = fromJust x : catMaybes xs
+    | otherwise = catMaybes xs
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe x Nothing = x
+fromMaybe _ (Just x) = x
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (x : _) = Just x
+
+mapMaybe :: (a -> Maybe b) -> [a] -> [b]
+mapMaybe = undefined
+
+maybe :: b -> (a -> b) -> Maybe a -> b
+maybe = undefined
+
+maybeToList :: Maybe a -> [a]
+maybeToList = undefined
+
+tryToModifyWith :: [Maybe (a -> a)] -> [a] -> [a]
+tryToModifyWith = undefined
+
